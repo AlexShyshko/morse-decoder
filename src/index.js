250241d8757a4f185ecf391ae607a87e41fc1da3
AlexShyshko <shish1988@mail.ru>
2021-07-11 00:13:23 +0300

    feat: write function decode

--- src/index.js
+++ src/index.js
@@ -38,7 +38,59 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let splittedArr = [];
+    let slicedStr = '';
+    let i = 0;
+
+    for (i = 0; i < expr.length; i = i + 10) {
+        slicedStr = expr.slice(i, i + 10);
+        splittedArr.push(slicedStr);
+    }
+
+    let slicedArr = [];
+    slicedArr = splittedArr.map(function(str) {
+        if (str === '**********') {
+            return slicedStr = ' ';
+        }
+        slicedStr = str;
+        i = 0;
+        while (str[i] === '0') {
+            slicedStr = str.slice(i + 2);
+            i = i + 2;
+        }
+        return slicedStr;
+    })
+
+    let convertedArr = slicedArr.map(function(str) {
+        let convertedStr = '';
+        if (str === ' ') {
+            return str;
+        }
+        i = 0;
+        while (i < str.length) {
+            if (str[i + 1] === '0') {
+                convertedStr = convertedStr + '.';
+            } else {
+                convertedStr = convertedStr + '-';
+            }
+            i = i + 2;
+        }
+        return convertedStr;
+    })
+
+    let decodedArr = [];
+    for (i = 0; i < convertedArr.length; i ++) {
+        if (convertedArr[i] === ' ') {
+            decodedArr.push(' ');
+        }
+        for (let key in MORSE_TABLE) {
+            let comparedKey = key;
+            if (comparedKey === convertedArr[i]) {
+                decodedArr.push(MORSE_TABLE[comparedKey]);
+            }
+        }
+    }
+    return decodedArr.join('');
 }
 
 module.exports = {
